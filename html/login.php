4d47a1fd6038da5fa79e1edd1d3ef09ad88160c1
Hassan Abdulmalik <hassanabdulmalik2258@gmail.com>
2023-03-13 10:58:42 +0100

    new

--- html/login.php
+++ html/login.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Propitix Tooling Login</title>
+	<title>Propitixs Tooling Login</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
