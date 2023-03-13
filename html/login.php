ae0c49e7a504f0cefb96e266df87d3ecfce581a8
Hassan Abdulmalik <hassanabdulmalik2258@gmail.com>
2023-03-13 13:07:29 +0100

    test

--- html/login.php
+++ html/login.php
@@ -2,12 +2,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Propitix Toolings Login</title>
+	<title>Propitix Tooling Login</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div class="header">
-		<h2>Login</h2>
+		<h2>Logins</h2>
 	</div>
 	<form method="post" action="login.php">
 
@@ -18,11 +18,11 @@
 			<input type="text" name="username" >
 		</div>
 		<div class="input-group">
-			<label>Password</label>
+			<label>Passwosrd</label>
 			<input type="password" name="password" pattern=".{4,8}" placeholder=" Password Must Be 4 Characters" >
 		</div>
 		<div class="input-group">
-			<button type="submit" class="btn" name="login_btn">Login</button>
+			<button type="submit" class="btn" name="login_btn">Loginss</button>
 		</div>
 	<!--	<p>
 			Not yet a member? <a href="register.php">Sign up</a>
